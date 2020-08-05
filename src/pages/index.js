8bb8365829825aa95ddac25d579581b540965994
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-08-05 19:38:47 +0530

    clean up gatsby starter template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,14 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import React from "react";
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  <div>
+    <h1 style={{ fontSize: "10rem" }}>Hi people</h1>
+    <p style={{ fontSize: "2rem", marginTop: "1rem" }}>
+      at. Mattis nunc sed blandit libero volutpat. Laoreet non curabitur gravida
+      arcu ac tortor dignissim convallis. Nibh praesent tristique magna sit amet
+      purus.
+    </p>
+  </div>
+);
 
-export default IndexPage
+export default IndexPage;
