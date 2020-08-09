35d55f5f8b97d1850f17c6f3b1b93fa7a6daca7c
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-08-10 02:11:50 +0530

    style: add images and code blocks to blog page

--- src/pages/one.jsx
+++ src/pages/one.jsx
@@ -1,10 +1,29 @@
-import React, { Fragment } from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "gatsby";
+import Prism from "prismjs";
 
 import Layout from "./../components/Layout";
 import Navbar from "./../components/Navbar";
 import Footer from "./../components/Footer";
+
+// The code we will be displaying
+const code = `
+const Validator = require("validator");
+
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+  
+	if (
+		!Validator.isEmpty(data.password) &&
+		!Validator.isLength(data.password, { min: 6, max: 30 })
+	) {
+		errors.password = "Password must be at least 6 chracters";
+	}
+
+	return { errors, isValid: isEmpty(errors) };
+};
+`;
 
 const HeroContainer = styled.div`
   color: var(--color-primary);
@@ -16,8 +35,8 @@ const HeroContainer = styled.div`
   justify-content: center;
 `;
 
-const HeroTitle = styled.div`
-  padding: 4rem 0;
+const HeroTitle = styled.hgroup`
+  padding: 2rem 0 4rem 0;
   max-width: 70rem;
   margin: 0 auto;
 `;
@@ -37,35 +56,78 @@ const BlogDate = styled.h2`
   margin-top: 3rem;
 `;
 
-const BlogContent = styled.div`
+const BlogContent = styled.article`
   /* Container styles */
+  & > * + * {
+    margin-top: 2.5rem;
+  }
+
   max-width: 50rem;
   margin: 0 auto;
   text-align: left;
-
-  & > * + * {
-    margin-top: 2rem;
-  }
-
   font-size: 1.1rem;
-  padding: 5rem 0;
+  font-kerning: normal;
+  padding: 3rem 0 5rem;
 
   /* Internal HTML styles */
 
   h1,
   h2 {
-    color: var(--color-text-head);
+    font-weight: 700;
   }
 
   h3,
   h4,
   h5,
   h6 {
-    color: var(--color-text-head-lower);
+    font-weight: 500;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    color: var(--color-text-head);
+    line-height: 130%;
+    margin: 2rem 0;
+  }
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  h2 {
+    font-size: 2.8rem;
+  }
+
+  h3 {
+    font-size: 2.6rem;
+  }
+
+  h4 {
+    font-size: 2.4rem;
+  }
+
+  h5 {
+    font-size: 2.2rem;
+  }
+
+  h6 {
+    font-size: 2rem;
   }
 
   p {
+    font-size: 1.1rem;
     color: var(--color-text-body);
+    line-height: 160%;
+  }
+
+  ol,
+  ul {
+    list-style-position: inside;
+    list-style-type: disc;
   }
 
   a,
@@ -77,14 +139,55 @@ const BlogContent = styled.div`
   }
 `;
 
+const ImageBox = styled.figure`
+  img {
+    display: block;
+    max-width: 100%;
+    margin: 0 auto;
+    border-radius: 10px;
+    box-shadow: 0px 10px 25px var(--color-box-shadow);
+  }
+`;
+
+const Code = ({ code }) => {
+  useEffect(() => {
+    // call the highlightAll() function to style our code blocks
+    Prism.highlightAll();
+  });
+
+  const CodeBox = styled.div`
+    box-shadow: 0px 5px 10px var(--color-box-shadow);
+    border-radius: 0.3em;
+    font-size: 1rem;
+
+    /* Prism plugins component */
+    .toolbar {
+      padding: 5px 15px 0 0;
+
+      & > .toolbar-item {
+        &:not(:last-child) {
+          margin-right: 5px;
+        }
+      }
+    }
+  `;
+
+  return (
+    <CodeBox>
+      <pre className="line-numbers">
+        <code className="language-javascript">{code}</code>
+      </pre>
+    </CodeBox>
+  );
+};
+
 function one() {
   return (
-    <Fragment>
-      <Layout>
+    <main>
+      <Layout as="header">
         <Navbar />
         <HeroContainer>
           <HeroTitle>
-            {/* <BlogTitle>Introducing Rekishi.</BlogTitle> */}
             <BlogTitle>
               What is hoisting but from a beginners perspective, not in any
               other language but in JavaScript?
@@ -95,7 +198,16 @@ function one() {
       </Layout>
       <Layout bgColor="var(--color-white)">
         <BlogContent>
-          <h1>Why API logging is a naive approach to API security?</h1>
+          {/* <h1>
+            Why API logging is a naive approach to API security, on this phase
+            of life?
+          </h1> */}
+          <h1>This is a Heading 1</h1>
+          <h2>This is a Heading 2</h2>
+          <h3>This is a Heading 3</h3>
+          <h4>This is a Heading 4</h4>
+          <h5>This is a Heading 5</h5>
+          <h6>This is a Heading 6</h6>
           <p>
             Raw API logs only contain the information pertaining to execute a
             single action. Usually the HTTP headers, IP address, request body,
@@ -106,6 +218,7 @@ function one() {
             elaborate behavioral flows that exercise your API in an unintended
             way.
           </p>
+          <Code code={"console.log('Hello World');"} />
           <p>
             In order to convert this to a user-centric model, we need to tag
             each event with user identifying information such as a tenant id, a
@@ -121,6 +234,7 @@ function one() {
             through visual inspection, static alert rules, or advanced anomaly
             detection.
           </p>
+          <Code code={code} />
           <p>
             Treat your API data like your database backups in that you never
             know when you might need them and should regularly test your system
@@ -135,6 +249,12 @@ function one() {
             handling GDPR requests such as right to erasure or right to access
             is a breeze.
           </p>
+          <ImageBox>
+            <img
+              src="https://images.unsplash.com/photo-1467779009031-53938b78ca38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+              alt="Via Unsplash"
+            />
+          </ImageBox>
           <h2>Detecting API security vulnerabilities</h2>
           <p>
             Unlike API logs for debugging purposes, these entities should be
@@ -144,10 +264,24 @@ function one() {
             down, then you lose access to valuable forensics data needed for
             auditing and postmortem review.
           </p>
+          <h4>Things that I love:</h4>
+          <ul>
+            <li>Coffee</li>
+            <li>Black tea</li>
+            <li>Green tea</li>
+            <li>Milk</li>
+          </ul>
+          <h3>Things that I love, but in ordered list:</h3>
+          <ol>
+            <li>Mango</li>
+            <li>Orange</li>
+            <li>Coconut</li>
+            <li>Strawberry</li>
+          </ol>
         </BlogContent>
       </Layout>
       <Footer bgColor="var(--color-white)" />
-    </Fragment>
+    </main>
   );
 }
 
