5978e8b9b1f3204e50fae350783e9fa6b1f92f74
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-08-21 17:01:16 +0530

    update siteurl on gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
                   Tulsi Prasad.`,
     keywords:
       "Tulsi Prasad, Wirescript, Beginners Web Development, React Redux, Python programming",
-    siteUrl: `https://wirescript.heytulsiprasad.vercel.app/`,
+    siteUrl: `https://wirescript.now.sh/`,
     author: {
       name: `Tulsi Prasad`,
     },
